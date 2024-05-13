1317c398218d4c3893409fe0bea044db7e5093ff
Akash Meheta <akashmeheta11@gmail.com>
2024-05-13 09:59:11 +0530

    Add CRED Operations

---
+++ src/appwrite/services.js
@@ -0,0 +1,134 @@
+import config from '../config/config.js';
+import { Client,  ID, Databases, Storage, Query } from "appwrite";
+
+export class Service{
+    client = new Client();
+    databases;
+    bucket;
+
+    constructor(){
+        this.client
+            .setEndpoint(config.appwriteurl)
+            .setProject(config.appwriteProjectId);
+        this.databases =  new Databases(this.client);
+        this.bucket = new Storage(this.client);
+    }
+
+    //Post Services
+
+    async createPost({title, slug, content, featuredImage, status, userId}){
+        try {
+            return await this.databases.createDocument(
+                config. appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId,
+                }
+            )
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async updatePost(slug, {title,content, featuredImage, status, userId}){
+        try {
+            return await this.databases.updateDocument(
+                config. appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId,
+                }
+            )
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async deletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                config. appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+            )
+            return true;
+        } catch (error) {
+            console.log("Appwrite Error::", error);
+            return false;
+        }
+    }
+
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(
+                config. appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+            )
+        } catch (error) {
+            console.log("Appwrite Error::", error);
+            return false;
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status", "active")]){
+        try {
+            return await this.databases.getDocument(
+                config. appwriteDatabaseId,
+                config.appwriteCollectionId,
+                queries,
+            )
+        } catch (error) {
+            console.log("Appwrite Error::", error);
+            return false;
+        }
+    }
+
+    //File Services
+
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                config.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite Error::", error);
+        }
+    }
+
+    async deleteFile(fileId){
+        try {
+            await this.bucket.deleteFile(
+                config.appwriteBucketId,
+                fileId
+            )
+            return true;
+        } catch (error) {
+            console.log("Appwrite Error::", error);
+            return false;
+        }
+    }
+
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            config.appwriteBucketId,
+            fileId
+        )
+    }
+}
+
+const service = new Service();
+
+export default service;
