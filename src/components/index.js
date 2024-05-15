6306281a027a16070438eb8aa1531fee7d3b43c9
Akash Meheta <akashmeheta11@gmail.com>
2024-05-15 09:43:58 +0530

    Adding Components

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,13 @@
 import { Footer } from "./Footer/Footer";
 import Header from "./Header/Header";
+import Container from "../components/container/Container";
+import Logo from "../components/Logo"
+import LogoutBtn from "../components/Header/LogoutBtn"
 
-export {Footer, Header};
+export {
+    Footer, 
+    Header,
+    Container,
+    Logo,
+    LogoutBtn
+};
