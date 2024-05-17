d44fc5dbaa85fd3cc894539e16c6dfad980a24ec
Akash Meheta <akashmeheta11@gmail.com>
2024-05-17 10:00:24 +0530

    Add Real Time Editor using TinyMCE and Post form

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import Header from "./Header/Header";
 import Container from "../components/container/Container";
 import Logo from "../components/Logo"
 import LogoutBtn from "../components/Header/LogoutBtn"
+import RTE from "./RTE";
 
 export {
     Footer, 
     Header,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE
 };
